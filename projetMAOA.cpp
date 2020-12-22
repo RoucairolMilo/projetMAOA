271105f79c53264a8c7649960d39871cd5c38221
Milo <milo.roucairol@gmail.com>
2020-12-22 22:55:11 +0100

    VRP mais bcp d'adaptations à faire

--- projetMAOA.cpp
+++ projetMAOA.cpp
@@ -9,6 +9,8 @@
 #include <sstream>
 #include <vector>
 
+#include <algorithm>
+
 #define epsilon 0.00001
 
 using namespace std;
@@ -18,15 +20,160 @@ typedef IloArray<IloNumVarArray> NumVarMatrix;
 
 ILOSTLBEGIN
 
-float solveLSPHeuristicPart1(vector<vector<int>> SC, vector<vector<int>> d, vector<int> L, vector<int> h, int u, int f, int C) {
+//mettre tout ça dans un autre fichier après
+// il faudra aussi modifier les entrées et sorties pour que ça marche bien avec les fonctions de lecture d'instances
+
+IloNum solveVRPMTZHeuristicPart1(vector<int> Cli, int m, vector<int> d, int Q, vector<vector<int>> cij) {
+	/*
+	Cli la list des clients, peut être leur coordonnées plutot ?
+	
+	
+	*/
+	//////////////
+	//////  CPLEX INITIALIZATION
+	//////////////
+
+	IloEnv   env;
+	IloModel model(env);
+	IloRangeArray c(env);
+
+	////////////////////////
+	//////  VAR
+	////////////////////////
+	ostringstream cstname;
+
+	//tous les arcs entre tous les clients et le productuer
+	vector<vector<IloNumVar>> x;
+	x.resize(Cli.size());
+	for (unsigned i = 0; i < Cli.size(); i++) {
+		x[i].resize(Cli.size());
+		for (unsigned j = 0; j < Cli.size(); j++) {
+			if (i == j) {
+				//pour s'assurer que l'arête sur soi ne serve à rien
+				x[i][j] = IloNumVar(env, 0.0, 0.0, ILOINT);
+			}
+			else {
+				x[i][j] = IloNumVar(env, 0.0, 1, ILOINT);
+			}
+			cstname.str("");
+			cstname << "x" << i << "_" << j;
+			x[i][j].setName(cstname.str().c_str());
+		}
+	}
+
+	vector<IloNumVar> w;
+	w.resize(Cli.size());
+	for (unsigned i = 0; i < Cli.size(); i++) {
+		w[i] = IloNumVar(env, 0, Q, ILOFLOAT);
+		ostringstream cstname;
+		cstname.str("");
+		cstname << "w" << i;
+		w[i].setName(cstname.str().c_str());
+	}
+
+	//////////////
+	//////  CST
+	//////////////
+
+	IloRangeArray CC(env);
+	int nbcst = 0;
+
+	//contrainte 6 (numérotés par rapport au sujet)
+	IloExpr c6(env);
+	for (unsigned j = 1; j < Cli.size(); j++)
+		c6 += x[0][j];
+	CC.add(c6 <= m);
+	cstname.str("");
+	cstname << "Cst_6";
+	CC[nbcst].setName(cstname.str().c_str());
+	nbcst++;
+
+	//contrainte 7
+	IloExpr c7(env);
+	for (unsigned j = 1; j < Cli.size(); j++)
+		c7 += x[j][0];
+	CC.add(c7 <= m);
+	cstname.str("");
+	cstname << "Cst_7";
+	CC[nbcst].setName(cstname.str().c_str());
+	nbcst++;
+
+	//contrainte 8
+	for (unsigned i = 1; i < Cli.size(); i++) {
+		IloExpr c8(env);
+		for (unsigned j = 1; j < Cli.size(); j++)
+			c8 += x[i][j];
+		CC.add(c8 <= 1);
+		cstname.str("");
+		cstname << "Cst_8col_" << i;
+		CC[nbcst].setName(cstname.str().c_str());
+		nbcst++;
+	}
+
+	//contrainte 9, presque pareil
+	for (unsigned i = 1; i < Cli.size(); i++) {
+		IloExpr c9(env);
+		for (unsigned j = 1; j < Cli.size(); j++)
+			c9 += x[j][i];
+		CC.add(c9 <= 1);
+		cstname.str("");
+		cstname << "Cst_9col_" << i;
+		CC[nbcst].setName(cstname.str().c_str());
+		nbcst++;
+	}
+
+	//contrainte 10 
+	for (unsigned i = 1; i < Cli.size(); i++) {
+		
+		for (unsigned j = 1; j < Cli.size(); j++) {
+			IloExpr c10(env);
+			c10 += w[i] - w[j] - (Q - d[i]) * x[i][j];
+			CC.add(c10 >= -Q);
+			ostringstream cstname;
+			cstname.str("");
+			cstname << "Cst_11col_" << i << "_line_" << j;
+			CC[nbcst].setName(cstname.str().c_str());
+			nbcst++;
+		}
+	}
+
+	//////////////
+	////// OBJ
+	//////////////
+
+
+	IloObjective obj = IloAdd(model, IloMinimize(env, 0.0));
+
+	for (unsigned i = 0; i < Cli.size(); i++) {
+		for (unsigned j = 0; j < Cli.size(); j++) {
+			obj.setLinearCoef(x[i][j], cij[i][j]);
+		}
+	}
+
+	///////////
+	//// RESOLUTION
+	//////////
+
+	IloCplex cplex(model);
+
+	cplex.solve();
+	std::cout << cplex.getObjValue();
+
+	//TODO : return les valeurs dont on a besoin
+
+	return cplex.getObjValue(); // on doir aps retourner ça, mais plutot un paquet de valeurs (un vecteur de ilonum ?)
+}
+
+
+IloNum solveLSPHeuristicPart1(vector<vector<int>> SC, vector<vector<int>> d, vector<int> L, vector<int> h, int u, int f, int C) {
 	/*
 
 
 
 	C : capacité de prodution
-	*/
 
-	int a = 2; //inutile
+	Devra retourner un vecteur avec les variables plutot que la valeur optimisée seulement.
+	*/
 
 
 	//Le LSP pour l'heuristique en CPLEX
@@ -41,8 +188,8 @@ float solveLSPHeuristicPart1(vector<vector<int>> SC, vector<vector<int>> d, vect
 
 
 	//////////////
-  //////  CPLEX INITIALIZATION
-  //////////////
+	//////  CPLEX INITIALIZATION
+	//////////////
 
 
 	IloEnv   env;
@@ -55,8 +202,8 @@ float solveLSPHeuristicPart1(vector<vector<int>> SC, vector<vector<int>> d, vect
 	//////  VAR
 	////////////////////////
 
-	int n = 10;
-	int l = 10;
+	int n = 10; //nombre de clients+producteur
+	int l = 10; //horizon
 
 
 	// c'est pas une var, mais je le définis ici :
@@ -72,7 +219,7 @@ float solveLSPHeuristicPart1(vector<vector<int>> SC, vector<vector<int>> d, vect
 			}
 		}
 
-		M[t] = min(C, sum);
+		M[t] = std::min(C, sum);
 	}
 
 	vector<IloNumVar> p;
@@ -267,6 +414,13 @@ float solveLSPHeuristicPart1(vector<vector<int>> SC, vector<vector<int>> d, vect
 	//////////
 
 	IloCplex cplex(model);
+
+	cplex.solve();
+	std::cout << cplex.getObjValue();
+
+	//TODO : return les valeurs dont on a besoin
+
+	return cplex.getObjValue(); // on doir aps retourner ça, mais plutot un paquet de valeurs
 }
 
 
@@ -274,7 +428,7 @@ float solveLSPHeuristicPart1(vector<vector<int>> SC, vector<vector<int>> d, vect
 int main()
 {
 
-
+	std::cout << "Hello World!";
 }
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
